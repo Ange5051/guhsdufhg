ca8b7e5f83077febb01d79160971a7310b25b27d
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2024-10-22 11:17:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Задача 1. Составить программный код вычисления площади треугольника со сторонами a, b, c, если он существует; в противном случае – вывести сообщение о том, что треугольника нет.
+            //Console.Write(("Введете первую сторону - "));
+            //double side1 = double.Parse(Console.ReadLine());
+            //Console.Write(("Введете вторую сторону - "));
+            //double side2 = double.Parse(Console.ReadLine());
+            //Console.Write(("Введете третью сторону - "));
+            //double side3 = double.Parse(Console.ReadLine());
+            //double p = ((side1 + side2 + side3)) / 2;
+            //if (side3 < side1 + side2 && side1 < side2 + side3 && side2 < side1 + side3)
+            //{
+            //    Console.WriteLine(("Треугольник существует"));
+            //    Console.WriteLine(($"Площадь треугольника ровна = {Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3)):f2}"));
+            //}
+            //else { Console.WriteLine(("Треугольник не существует")); }
+            //Console.Read();
+
+            ////Задача 2 Составить программный код решения квадратного уравнения ax2+bx+c=0.
+            //Console.Write("a = ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b = ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c = ");
+            //double c = double.Parse(Console.ReadLine());
+            //double d = Math.Pow(b, 2) - 4 * a * c;
+            //if (d > 0)
+            //{
+            //    double x1 = (-b + Math.Sqrt(d)) / 2 * a;
+            //    double x2 = (-b - Math.Sqrt(d)) / 2 * a;
+            //    Console.Write($"Дискриминант имеет два корня\nx1 = {x1:f2}\nx2= {x2:f2}");
+            //}
+            //else if (d == 0)
+            //{
+            //    double x0 = -b / 2 * a;
+            //    Console.Write($"Дискриминант имеет один корень\nx1 = {x0:f2}");
+            //}
+            //else
+            //{
+            //    Console.Write("Нет корней");
+            //}
+            //Console.Read();
+
+
+        }
+    }
+}
